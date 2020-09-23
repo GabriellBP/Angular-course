375e014a0862c277b81da7b90137d310c112addb
gabriel <gbp@ic.ufal.br>
2020-09-23 17:01:16 -0300

    add my first component

---
+++ event-manager/src/app/first/first.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  template: `
+    <p>Hello world! This is my first component!</p>
+  `
+})
+export class FirstComponent{
+
+  constructor() { }
+
+}

a3a00d2c116a617557806dbcfabcc80dd70276b7
Lucas Amorim <amorimlucas1998@gmail.com>
2020-10-05 23:40:03 -0300

    step 3: ngClass

--- classes-content/class-two/src/app/app.module.ts
+++ classes-content/class-two/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgifDirectiveComponent } from './ngif-directive/ngif-directive.component';
 import { NgforDirectiveComponent } from './ngfor-directive/ngfor-directive.component';
+import { NgclassDirectiveComponent } from './ngclass-directive/ngclass-directive.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     NgifDirectiveComponent,
-    NgforDirectiveComponent
+    NgforDirectiveComponent,
+    NgclassDirectiveComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

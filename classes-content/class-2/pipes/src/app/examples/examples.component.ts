1200b376e7deb0abfac3665b2f53cc9187f1af5b
gabriel <gbp@ic.ufal.br>
2020-10-07 11:49:27 -0300

    step 1: create project and test some angular pipes

---
+++ classes-content/class-2/pipes/src/app/examples/examples.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-examples',
+  templateUrl: './examples.component.html',
+  styleUrls: ['./examples.component.css']
+})
+export class ExamplesComponent {
+
+  book: any = {
+    'title': 'Essencialismo',
+    'rating': 4.8125,
+    'pagesNumber': 272,
+    'price': 33.86,
+    'releaseDate': new Date(2015, 4, 15),
+    'url': 'https://amzn.to/2Gr52oJ'
+  };
+
+  constructor() { }
+
+}

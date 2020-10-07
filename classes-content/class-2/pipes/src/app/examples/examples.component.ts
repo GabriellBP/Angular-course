93a63486e070f00a302f14281a500bddb332bfa5
gabriel <gbp@ic.ufal.br>
2020-10-07 11:59:17 -0300

    step 2: create a pipe

--- classes-content/class-2/pipes/src/app/examples/examples.component.ts
+++ classes-content/class-2/pipes/src/app/examples/examples.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ExamplesComponent {
 
   book: any = {
-    'title': 'Essencialismo',
+    'title': 'livro essencialismo',
     'rating': 4.8125,
     'pagesNumber': 272,
     'price': 33.86,

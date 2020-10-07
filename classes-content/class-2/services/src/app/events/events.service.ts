6cfe99cae9a90018ed59893921dd588cb52f2f1f
gabriel <gbp@ic.ufal.br>
2020-10-06 22:45:24 -0300

    step 4: inject a service in another service

--- classes-content/class-2/services/src/app/events/events.service.ts
+++ classes-content/class-2/services/src/app/events/events.service.ts
@@ -1,4 +1,5 @@
 import {EventEmitter, Injectable} from '@angular/core';
+import {LogService} from '../shared/log.service';
 
 @Injectable()
 export class EventsService {
@@ -9,15 +10,19 @@ export class EventsService {
 
   events: string[] = ['Event 1', 'Event 2', 'Event 3'];
 
-  constructor() {
+  constructor(private logService: LogService) {
     console.log('Events Service');
   }
 
   getEvents(): string[] {
+    this.logService.consoleLog('getting the events');
+
     return this.events;
   }
 
   addEvent(value: string): void {
+    this.logService.consoleLog(`adding a new event called ${value}`);
+
     this.events.push(value);
 
     this.eventEmitter.emit(value);

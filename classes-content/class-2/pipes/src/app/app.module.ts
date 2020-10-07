e7bfb54032702a54461c004e06e675265b582908
gabriel <gbp@ic.ufal.br>
2020-10-07 11:59:17 -0300

    step 2: create a pipe

--- classes-content/class-2/pipes/src/app/app.module.ts
+++ classes-content/class-2/pipes/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ExamplesComponent } from './examples/examples.component';
+import { CamelCasePipe } from './camel-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExamplesComponent
+    ExamplesComponent,
+    CamelCasePipe
   ],
   imports: [
     BrowserModule

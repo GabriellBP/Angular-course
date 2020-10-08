1c0a47980a290b58904453af6103805e9d4f1817
Lucas Amorim <amorimlucas1998@gmail.com>
2020-10-08 15:23:41 -0300

    step 10: using child routes

--- classes-content/class-three-routing/src/app/students/students-routing.module.ts
+++ classes-content/class-three-routing/src/app/students/students-routing.module.ts
@@ -5,10 +5,12 @@ import {StudentDetailComponent} from "./student-detail/student-detail.component"
 import {StudentFormComponent} from "./student-form/student-form.component";
 
 const routes: Routes = [
-  {path: 'students', component: StudentsComponent},
-  {path: 'students/new', component: StudentFormComponent},
-  {path: 'students/:id/edit', component: StudentFormComponent},
-  {path: 'students/:id', component: StudentDetailComponent}
+  {path: 'students', component: StudentsComponent, children: [
+      {path: 'new', component: StudentFormComponent},
+      {path: ':id/edit', component: StudentFormComponent},
+      {path: ':id', component: StudentDetailComponent}
+    ]},
+
 ];
 
 @NgModule({

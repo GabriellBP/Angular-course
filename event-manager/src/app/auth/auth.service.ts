05ca1beb16bbaba23e916d5c2cf2d800cafc4fdb
Lucas Amorim <amorimlucas1998@gmail.com>
2020-10-17 15:03:30 -0300

    implement login screen, making layouts more generic

---
+++ event-manager/src/app/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  isLoggedIn = false;
+
+  constructor(private router: Router) { }
+
+  doLogout() {
+    this.isLoggedIn = false;
+    this.router.navigate(['/login']);
+  }
+
+  doLogin(username, password, returnUrl) {
+    if (username == 'asus' && password == '123456') {
+      this.isLoggedIn = true;
+      if (!returnUrl)
+        this.router.navigate(['/eventos']);
+      else
+        this.router.navigateByUrl(returnUrl);
+      return true;
+    }
+    return false;
+  }
+}

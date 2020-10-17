a01b1513ca63070e21fd7dbb96f89d07a55f672f
Lucas Amorim <amorimlucas1998@gmail.com>
2020-10-17 15:48:16 -0300

    add tag module using lazy loading, fix previos problem of calculated height: min-height was auto

--- event-manager/src/app/events/events-routing.module.ts
+++ event-manager/src/app/events/events-routing.module.ts
@@ -4,10 +4,12 @@ import {EventsComponent} from './events.component';
 import {EventListComponent} from '../event-list/event-list.component';
 import {EventListModule} from '../event-list/event-list.module';
 import {AuthGuard} from '../auth/auth.guard';
+import {TagsModule} from '../tags/tags.module';
 
 const routes: Routes = [
   {path: '', component: EventsComponent, canActivate: [AuthGuard], children: [
       {path: 'events', loadChildren: () => EventListModule},
+      {path: 'tags', loadChildren: () => TagsModule},
       {path: '', redirectTo: 'events', pathMatch: 'full'}
     ]}
 ];
